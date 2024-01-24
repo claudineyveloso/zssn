c4906c61485a95c582e4b583ff7ef118c2a466f1
Claudiney Veloso <claudineyveloso@gmail.com>
2024-01-24 17:30:57 -0300

    added quantity field in the item entity

--- db/migrate/20240124171507_create_inventories.rb
+++ db/migrate/20240124171507_create_inventories.rb
@@ -8,6 +8,7 @@ class CreateInventories < ActiveRecord::Migration[7.1]
     create_table :inventories do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
+      t.integer :quantity
 
       t.timestamps
     end
