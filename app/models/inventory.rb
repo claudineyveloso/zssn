7b8b989f98ddd9365495aa75d59169f71d5f0ae8
Claudiney Veloso <claudineyveloso@gmail.com>
2024-01-26 18:58:21 -0300

    created test inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -22,6 +22,7 @@
 class Inventory < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  validates :user_id, presence: true
   validates :item_id, presence: true
   validates :quantity, presence: true
 end
