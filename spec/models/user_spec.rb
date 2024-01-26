0e43e4155172ba8d6a0457677d15be1aebcaf6b7
Claudiney Veloso <claudineyveloso@gmail.com>
2024-01-26 18:01:00 -0300

    created test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:inventories) }
+    it do
+      should have_many(:reporteds)
+        .class_name('Infected')
+        .dependent(:destroy)
+        .with_foreign_key('user_id_reported')
+    end
+
+    it do
+      should have_many(:notifieds)
+        .class_name('Infected')
+        .dependent(:destroy)
+        .with_foreign_key('user_id_notified')
+    end
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:age) }
