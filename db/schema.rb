caab4196c32bdc1cfb9844e1fa0c478c9754d32e
Claudiney Veloso <claudineyveloso@gmail.com>
2024-02-12 11:33:56 -0300

    changed schemam of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_11_205255) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_044654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_205255) do
     t.string "longitude", null: false
     t.boolean "infected", default: false
     t.integer "contamination_notification", default: 0
-    t.boolean "is_active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
