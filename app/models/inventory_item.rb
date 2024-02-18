c570b3c3d81a6bf1acf46ec4fa17c2df38eb3bf2
Claudiney Veloso <claudineyveloso@gmail.com>
2024-02-18 16:32:24 -0300

    created method average_quantity_per_user

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -24,4 +24,25 @@
 class InventoryItem < ApplicationRecord
   belongs_to :inventory
   belongs_to :item
+
+  def self.average_quantity_per_user
+    users_data = []
+    # For each user, calculate the average quantity of each item
+    User.includes(:inventory, :inventory_items).where(infected: false).find_each do |user|
+      next unless user.inventory.present? && user.inventory.inventory_items.present?
+
+      inventory_items = user.inventory.inventory_items.group(:item_id).sum(:quantity)
+      user_data = {
+        name: user.name,
+        items: {}
+      }
+      inventory_items.each do |item_id, total_quantity|
+        item_description = Item.find(item_id).description
+        user_data[:items][item_description] = total_quantity
+      end
+      users_data << user_data
+    end
+    # average_quantity_per_user.to_json
+    { data: { users: users_data } }
+  end
 end
