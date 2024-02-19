c58bbd3e71aec5b1c023217d2a081eaa8aa34d90
Claudiney Veloso <claudineyveloso@gmail.com>
2024-02-19 17:37:19 -0300

    configuration of gem rswag

---
+++ config/initializers/rswag.rb
@@ -0,0 +1,7 @@
+Rswag::Api.configure do |config|
+  config.openapi_root = Rails.root.join('swagger').to_s
+end
+
+Rswag::Ui.configure do |config|
+  config.openapi_endpoint '/swagger.yaml', 'API V1 Docs'
+end
