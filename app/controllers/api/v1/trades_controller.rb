2b2f6f53444ee258ab844b9b8b05b57c436929b4
Claudiney Veloso <claudineyveloso@gmail.com>
2024-02-19 09:40:04 -0300

    created Trades Controller

--- app/controllers/api/v1/trades_controller.rb
+++ app/controllers/api/v1/trades_controller.rb
@@ -4,47 +4,23 @@ module Api
   module V1
     # TradesController
     class TradesController < ApplicationController
-      before_action :set_user
       def create
-        user_giver = User.find(trade_params[:user_giver])
-        user_receiver = User.find(trade_params[:user_receiver])
-        items_giver = Item.find(trade_params[:items_giver])
-        items_receiver = Item.find(trade_params[:item_receiver])
-
-        if valid_trade?(user_giver, items_giver, user_receiver, items_receiver)
-          Trade.transaction do
-            items_giver.each { |item| exchange_items(user_giver, user_receiver, item) }
-            items_receiver.each { |item| exchange_items(user_receiver, user_giver, item) }
-            items
-          end
-          render json: { message: 'Trade successful' }, status: :ok
+        trade = Trade.new(trade_params)
+        if trade.valid?
+          trade.execute_trade
+          render json: { message: 'Troca realizada com sucesso' }, status: :ok
         else
-          render json: { message: 'Invalid trade request' }, status: :unprocessable_entity
+          render json: { error: trade.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       private
 
       def trade_params
-        params.require(:trade).permit(:user_giver, :user_receiver, items_giver: [], item_receiver: [])
-      end
-
-      def valide_trade?(user_giver, items_giver, user_receiver, items_receiver)
-        total_score(user_giver, items_giver) == total_score(user_receiver, items_receiver)
-      end
-
-      def total_score(_user, items)
-        # items.sum {|item| item.score * params[`"quantity_#{item.id}".to_sym].to_i }
-        items.sum { |item| item.score * params[`"quantity_#{item.id}"`].to_i }
-      end
-
-      def exchange_items(giver, receiver, item)
-        giver.inventory.remove_item(item)
-        receiver.inventory.remove_item(item)
-      end
-
-      def set_user
-        User.find(params[:user_id])
+        params.require(:trade).permit(:user_giver,
+                                      :user_receiver,
+                                      item_user_giver: {},
+                                      item_user_receiver: {})
       end
     end
   end
